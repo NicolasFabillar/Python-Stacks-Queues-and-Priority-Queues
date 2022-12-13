e2759486d50009cb246b9c8ca39fea7d88717865
NicolasFabillar <nicosfabillar09@gmail.com>
2022-12-13 15:10:45 +0800

    Python code for determining how python compares tuples.

---
+++ Python comparison logic on tuples - Logic on priority queue.py
@@ -0,0 +1,9 @@
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+# Comparing the values to determine how python compares tuples.
+print(person1 < person2)
+
+print(person2 < person3)
