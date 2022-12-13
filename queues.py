cf87cf8eea73de9ed9efe272e83e30cdc65ce926
NicolasFabillar <nicosfabillar09@gmail.com>
2022-12-13 14:04:34 +0800

    Upgraded it so that it can accept multiple elements initially.

--- queues.py
+++ queues.py
@@ -3,8 +3,9 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    # this make it so that it can accept multiple elements initially or it can create deque without elements.)
+    def __init__(self, *element):
+        self._elements = deque(element)
 
     def enqueue(self, element):
         self._elements.append(element)
