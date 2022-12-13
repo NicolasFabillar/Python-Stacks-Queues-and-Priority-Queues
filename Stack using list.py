b0bf8da9225b04a7af048df5c8c7953cd12b4499
NicolasFabillar <nicosfabillar09@gmail.com>
2022-12-13 14:42:40 +0800

    Tried Stack using List. Works simmilar.

---
+++ Stack using list.py
@@ -0,0 +1,9 @@
+lifo = [] #this is list not deque. This does not need any explanation anymore haha.
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
