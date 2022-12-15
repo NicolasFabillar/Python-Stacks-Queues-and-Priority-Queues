231d42dabb1f890790767cf7e1dbff6cb8153cc0
NicolasFabillar <nicosfabillar09@gmail.com>
2022-12-15 17:39:23 +0800

    A program that find the shortest path using Python deque collection with a fast append operation.

--- Queues in Practice - Shortest Path - 1.py
+++ Queues in Practice - Shortest Path - 1.py
@@ -4,7 +4,7 @@ from graph import City, load_graph
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 city1 = nodes["aberdeen"]
-city2 = nodes["perth"]
+city2 = nodes["bristol"]
 
 for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
     print(f"{i}.", " → ".join(city.name for city in path))
