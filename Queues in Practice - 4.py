6effaefca6f3c633fb7750abb62a320ca283e092
NicolasFabillar <nicosfabillar09@gmail.com>
2022-12-15 15:51:43 +0800

    Using .neighbors() method on a graph we listed the list of neighbor city or node.

--- Queues in Practice - 4.py
+++ Queues in Practice - 4.py
@@ -1,7 +1,6 @@
 from graph import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
-print(nodes["bangor"])
 
-print()
-print(graph)
+for neighbor in graph.neighbors(nodes["london"]):
+    print(neighbor.name)
